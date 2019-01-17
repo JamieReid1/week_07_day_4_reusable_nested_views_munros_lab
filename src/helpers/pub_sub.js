5adbaa6e275599f98e3878c163a5b59447a1dce6
Jamie Reid <wjreid85.jr@gmail.com>
2019-01-17 13:31:03 +0000

    Sets up initial files.

---
+++ src/helpers/pub_sub.js
@@ -0,0 +1,16 @@
+const PubSub = {
+  publish: function (channel, payload) {
+    console.log(`Published on channel ${channel}, payload: ${payload}`);
+    const event = new CustomEvent(channel, {
+      detail: payload
+    });
+    document.dispatchEvent(event);
+  },
+
+  subscribe: function (channel, callback) {
+    console.log(`Subscribed to channel: ${channel}`);
+    document.addEventListener(channel, callback);
+  }
+};
+
+module.exports = PubSub;
